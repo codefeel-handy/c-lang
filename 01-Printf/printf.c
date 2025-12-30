b5479c5bde2adecd6b429b8da0fcda525dd5b4ab
CodeJamLts <ho9udata@naver.com>
2025-12-31 00:07:26 +0900

    01-Printf 코드 정리

---
+++ 01-Printf/printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* 문자열 출력 */
+    printf("Say Hello~, OverTech!\n");
+
+    /* 문자열 출력 */
+    printf("하이 안녕~, 오버테크!\n");
+
+    /* 정상 종료 */
+    return 0;
+}
